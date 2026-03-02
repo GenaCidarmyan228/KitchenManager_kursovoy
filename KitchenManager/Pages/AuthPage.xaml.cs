9399cffd319ea5e3f116d7759160c40c7de2c8da
GenaCidarmyan228 <F1een@bk.ru>
2026-03-03 09:42:20 +1000

    fdg

--- KitchenManager/Pages/AuthPage.xaml.cs
+++ KitchenManager/Pages/AuthPage.xaml.cs
@@ -21,7 +21,7 @@ namespace KitchenManager.Pages
             if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password))
             {
                
-                MessageBox.Show("Введите логин и пароль!");
+                MessageBox.Show("Введите логин и пароль!", "Внимание!");
                 return;
             }
 
@@ -32,7 +32,7 @@ namespace KitchenManager.Pages
 
                 if (userObj == null)
                 {
-                    MessageBox.Show("Такого пользователя нет или пароль неверный!",
+                    MessageBox.Show( "Такого пользователя нет или пароль неверный!",
                                     "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
                 else
@@ -40,7 +40,7 @@ namespace KitchenManager.Pages
                    
                     App.CurrentUser = userObj;
 
-                    MessageBox.Show($"Добро пожаловать, {userObj.Roles.RoleName}!");
+                    MessageBox.Show($"Добро пожаловать, {userObj.Roles.RoleName}!",  "Внимание!");
                     NavigationService.Navigate(new OrdersPage());
                 }
             }
